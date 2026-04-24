c88bb1f73f7c0007eb23aabf91f5fba5c316e140
youngcan <wangyongkai03@meituan.com>
2026-04-24 14:27:04 +0800

    fix formula source url for v0.3.4

--- Formula/wyckoff.rb
+++ Formula/wyckoff.rb
@@ -3,7 +3,7 @@ class Wyckoff < Formula
 
   desc "Wyckoff method quantitative analysis agent for A-shares"
   homepage "https://github.com/YoungCan-Wang/Wyckoff-Analysis"
-  url "https://files.pythonhosted.org/packages/source/y/youngcan-wyckoff-analysis/youngcan_wyckoff_analysis-0.3.4.tar.gz"
+  url "https://files.pythonhosted.org/packages/79/0b/b5725f634c6c5756164981fe21b05c07883919ebe944044d9cdbf436db19/youngcan_wyckoff_analysis-0.3.4.tar.gz"
   sha256 "307a31d44f31fa69530c1c4f27d758a7e0bc58d4a57f57073a076f855b21645a"
   license "AGPL-3.0-only"
 
